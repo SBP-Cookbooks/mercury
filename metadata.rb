4fc91628e0484d0cd3063cdc26547c3dcb5b31bf
Ronald Doorn <rdoorn@schubergphilis.com>
2018-02-19 17:15:40 +0100

    use new mercury release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rdoorn@schubergphilis.com'
 license          'Apache-2.0'
 description      'Installs/Configures mercury'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.1'
 chef_version     '>= 12.5' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/schubergphilis/mercury/issues'
