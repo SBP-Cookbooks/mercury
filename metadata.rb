6895c9447bd7e35832180599a33da3d5d9cbbf09
Ronald Doorn <rdoorn@schubergphilis.com>
2018-09-27 09:47:19 +0200

    new mercury version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rdoorn@schubergphilis.com'
 license          'Apache-2.0'
 description      'Installs/Configures mercury'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.3.4'
+version          '1.3.5'
 chef_version     '>= 12.5' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/sbp-cookbooks/mercury/issues'
