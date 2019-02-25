d5f269653153af3b92bbba7483d0ae27d3ea6301
Ronald Doorn <rdoorn@schubergphilis.com>
2019-02-25 12:32:16 +0100

    bump mercuy version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rdoorn@schubergphilis.com'
 license          'Apache-2.0'
 description      'Installs/Configures mercury'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.4.2'
+version          '1.4.3'
 chef_version     '>= 12.5' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/sbp-cookbooks/mercury/issues'
