22abec7e06f4c587b82bcdbd84ea5f137d64c477
Ronald Doorn <rdoorn@schubergphilis.com>
2019-08-30 23:19:11 +0200

    fix: Test automation7 (#29)
    * need sudo
    * testing with go
    * another attempt at getting go
    * use xenial instead
    * final

--- upload.sh
+++ upload.sh
@@ -35,11 +35,19 @@ case "${REF}" in
 esac
 newversion="${major}.${minor}.${patch}"
 
+echo "starting script..."
+env | grep TRAVIS
+gimme -l
+gimme 1.12.9
 # install a more recent Go than apt-get can supply
-curl https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz -o go.tar.gz
-sudo tar -xvzf go.tar.gz -C /usr/local/
-export GOROOT=/usr/local/go
-export PATH="$PATH:${GOROOT}/bin"
+#curl https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz -o go.tar.gz
+#sudo tar -xvzf go.tar.gz -C /usr/local/
+go version
+go env
+
+source /home/travis/.gimme/envs/*.env
+#export GOROOT=/usr/local/go
+#export PATH="$PATH:${GOROOT}/bin"
 
 echo "installing ghr"
 go get github.com/tcnksm/ghr
