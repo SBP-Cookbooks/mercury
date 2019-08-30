e6472015a35b00bd3d1178dcce08718bf508b830
Ronald Doorn <rdoorn@schubergphilis.com>
2019-08-31 00:20:30 +0200

    remove the find

--- upload.sh
+++ upload.sh
@@ -60,7 +60,6 @@ fi
 
 echo "new version to be created: old: ${oldversion} new: ${newversion}"
 
-find / -name "ghr"
 ~/go/bin/ghr -soft -t ${GITHUB_TOKEN} -u sbp-cookbooks -r mercury -c ${TRAVIS_COMMIT} -n "Mercury Cookbook v${newversion}" ${newversion}
 
 sed -e "s/version          '1.0.0'/version          '${newversion}'/" -i metadata.rb
