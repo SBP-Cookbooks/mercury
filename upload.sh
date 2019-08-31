513c635eb53a5f75a300892f2de0ccf5c9a782ad
Ronald Doorn <rdoorn@schubergphilis.com>
2019-08-31 10:23:59 +0200

    fix: final auto-commit (#33)
    * update
    
    * fix: testing
    
    * temp disable version check
    
    * this should be it

--- upload.sh
+++ upload.sh
@@ -45,7 +45,7 @@ gimme 1.12.9
 go version
 go env
 
-source /home/travis/.gimme/envs/*.env
+. /home/travis/.gimme/envs/*.env
 #export GOROOT=/usr/local/go
 #export PATH="$PATH:${GOROOT}/bin"
 
@@ -62,8 +62,7 @@ echo "new version to be created: old: ${oldversion} new: ${newversion}"
 
 mkdir tmpdir
 echo "executing ghr"
-find . -name "ghr"
-~/go/bin/ghr -soft -t ${GITHUB_TOKEN} -u sbp-cookbooks -r mercury -c ${TRAVIS_COMMIT} -n "Mercury Cookbook v${newversion}" ${newversion} ./tmpdir
+~/gopath/bin/ghr -soft -t ${GITHUB_TOKEN} -u sbp-cookbooks -r mercury -c ${TRAVIS_COMMIT} -n "Mercury Cookbook v${newversion}" ${newversion} ./tmpdir
 echo "ghr reported $?"
 
 sed -e "s/version          '1.0.0'/version          '${newversion}'/" -i metadata.rb
